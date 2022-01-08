d7756304c98379b189534b3a9a9d29459e8b6ba7
Hugo Granath <72066319+mrHugolo@users.noreply.github.com>
2022-01-08 22:10:13 +0100

    Initial commit

---
+++ demo/src/main/java/com/hugolo/demo/RoutinesApplication.java
@@ -0,0 +1,13 @@
+package com.hugolo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutinesApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/hugolo/demo/RoutinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugolo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
